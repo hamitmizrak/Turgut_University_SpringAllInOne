7bac44c4490a4910f55d94954d9b6ede475683ff
hamitmizrak <hamitmizrak@gmail.com>
2023-04-27 16:11:29 +0300

    CustomerEntity, ICustomerRepository,Delivered Query

--- src/main/java/com/hamitmizrak/data/repository/ICustomerRepository.java
+++ src/main/java/com/hamitmizrak/data/repository/ICustomerRepository.java
@@ -6,5 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ICustomerRepository  extends CrudRepository<CustomerEntity,Long> {
+
     //Delivered Query
+    CustomerEntity findByEmail(String email);
 }
